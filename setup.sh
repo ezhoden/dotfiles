a18f5c7545a827c4b3b050a2b25a0c03d726fb3e
Denis Ezhov <ezhoden@mail.ru>
2024-05-10 17:01:31 +0400

    move all configs to dotfiles repo, basic script for symlinking all configs

---
+++ setup.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+
+dotfiles="$HOME/dotfiles"
+
+configs=(
+	hypr
+	kitty
+	nvim
+	waybar
+)
+
+echo "Creating symlink for dotfiles"
+for value in "${configs[@]}"
+do
+	destination="$HOME/.config/$value"
+	if [ -d "$destination" ]; then echo "Directory $destination already exists, deleting it"; fi
+	if [ -d "$destination" ]; then rm -rf $destination; fi
+	ln -s $dotfiles/$value $HOME/.config/
+	echo "Symlink for $value has been created ('$dotfiles/$value' -> $destination)"
+done
+
